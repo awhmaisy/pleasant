77551d25343dc02955a5b09dd3b2ff2084696864
mei higashi <mei@meihigashi.com>
2024-11-19 19:17:20 -0800

    hey

---
+++ src/routes/+page.svelte
@@ -0,0 +1,72 @@
+<link rel="icon" type="image/jpg" href="icon.jpg" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="aura evangelist ( ᵘ ᵕ ᵘ ⁎)" />
+<title>maisyland</title>
+<h3>maisy</h3>
+<a href="mailto:mei@softwear.fund">mei@softwear.fund</a>
+<br />
+x.com/<a href="https://x.com/awhmaisy">awhmaisy</a>
+<br />
+<p></p>
+<p>
+    hai! i’m an ex-social media producer + manager, with expertise in relational
+    engineering and cult building. high-focus on distributed phygital hardware
+    in web3 and L2 applications in the creator economy (#sadge) 
+    <br /><br />
+    i dropped out of
+    3rd grade to research nanotechnology applications in oncology at 
+    <a href="https://tdg.ucla.edu/" target="_blank">
+        ucla BSC
+    </a>
+    and product/graphic design at 
+    <a href="https://www.artcenter.edu/" target="_blank">
+        art center college of design.
+    </a>
+    i wanted a locker and went to public high school.. got bored, played around with 400k+ on
+    tiktok, and cofounded an audiotechnical hardware company. 
+    <br /><br />
+    also been configuring a degree && building covenants within my social circles at usc’s
+    <a href="https://iovine-young.usc.edu/" target="_blank">
+        iovine and young academy
+    </a>
+    for a year. i’m in training to transform technical + creative engergies into businesses! 
+    <br /><br />
+    hahahhaa plot twist… i’m leaving to build
+    defense hard + software, among other things i’m yet to uncover. oh, and i’m
+    crafting apparel to add to the intriguing effect of your laptop stickers. pm
+    me if you’d like a 1-of-1 or if your startup needs cult merch. 
+    <br /><br />
+    i do believe that marketing / smm / universe creation can cultivate evangelism on a
+    psychological level. i'm a proponent of new-age publicity tactics. very
+    interested to see how my theorizations may evolve deeper into quantum
+    mechanics and reality scripting.
+</p>
+<br />
+<hr />
+<br />
+<h3>pieces</h3>
+<ul>
+    <li>
+        <a href="/blog/sept">sept</a>
+    </li>
+    <li>
+        <a href="/blog/oct">oct</a>
+    </li>
+</ul>
+<br />
+<hr />
+<br />
+<h3>lil joys as of late...</h3>
+<ul>
+    <li>reading papers <em>#drool</em></li>
+    <li>still learning lil programming</li>
+    <li>
+        selecting textile samples for <a
+            href="https://x.com/i/communities/1830091484231770285"
+            >cyberangels</a
+        >
+    </li>
+    <li>f#cking up a shopify backend :3</li>
+    <li>tooling for a commerical hardware proj</li>
+    <li>always doing diligence on smt stealthy</li>
+</ul>
